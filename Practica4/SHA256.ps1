2d2f7ec338c44a008939d75e098c4b64c87bfaf7
SilvestreMC <silvestre.martinezcs@uanl.edu.mx>
2021-03-21 15:24:44 -0600

    Primer commit

---
+++ Practica4/SHA256.ps1
@@ -0,0 +1,14 @@
+﻿#Script para comprobar que los archivos tengan SHA256
+
+
+Get-FileHash fcfm.png -Algorithm SHA256
+Write-Host "fcfm.png"
+
+Get-FileHash mystery_img1.txt -Algorithm SHA256
+Write-Host "mystery_img1.txt"
+
+Get-FileHash mystery_img2.txt -Algorithm SHA256
+Write-Host "mystery_img2.txt"
+
+Get-FileHash msg.txt -Algorithm SHA256
+Write-Host "msg.txt"
